93ddcd8bf65ad783ef238b7621d2c321d1c16057
mtaltunkanat <muhammedaltunkanat.kariyer@gmail.com>
2024-11-19 10:17:52 +0300

    Messages section active and finish project

--- react-chat-app/src/App.jsx
+++ react-chat-app/src/App.jsx
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app"
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
+import { collection, doc, getFirestore, orderBy, query, serverTimestamp, setDoc } from "firebase/firestore"
+import { useEffect, useRef, useState } from "react"
 
 import { useAuthState } from "react-firebase-hooks/auth"
+import { useCollection } from "react-firebase-hooks/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyAMDAhiPL7xC8F7KpLgxH8RXVT0Nu_b6VY",
@@ -22,7 +24,7 @@ const db = getFirestore(app)
 function App() {
   const [ user ] = useAuthState(auth)
   return (
-    <div className="max-w-6xl mx-auto">
+    <div className="max-w-6xl mx-auto flex flex-col h-screen">
 
       <header className="flex justify-between border-b p-2 mb-4">
         <h1 className="text-2xl">
@@ -32,7 +34,13 @@ function App() {
       </header>
 
       {
-        user ? <p>Hoş Geldiniz!</p> : <SignIn/> 
+        user ? 
+        <>
+        <MessageList/> 
+        <MessageForm/>
+        </>
+        : 
+        <SignIn/> 
       }
     </div>
   )
@@ -63,6 +71,91 @@ function SignIn(){
         className="bg-red-500 p-2 text-white rounded-lg">
           Çıkış Yap
         </button>
+      )
+    }
+
+    function MessageList(){
+      const messagesRef = collection(db, "messages")
+       const [ messages ] = useCollection(query(messagesRef, orderBy("createdAt")))
+
+       const listEndRef = useRef()
+
+       useEffect(() => {
+        listEndRef.current.scrollIntoView({ behavior: "smooth" })
+       }, [messages?.docs])
+
+       return(
+        <div className="overflow-y-auto h-full space-y-2 px-2">
+          {
+            messages?.docs.map((d) => (
+              <MessageItem key={d.id} data={d.data()} />
+            ))
+          }
+          <div ref={listEndRef}></div>
+        </div>
+      )
+    }
+    
+    function MessageItem(props){
+
+      const { text, userPhoto, userUid } = props.data
+      const reverseClass = auth.currentUser.uid === userUid ? "flex-row-reverse" : ""
+      
+      return(
+        <div  className={`flex ${reverseClass} items-center gap-1 w-full`}>
+                <img
+                src={userPhoto}
+                className="w-10 h-10 rounded-full"
+                >
+                </img>
+                <div className="bg-gray-200 p-2 rounded-full">
+                {text}
+                </div>
+              </div>
+      )
+    }
+
+    function MessageForm(){
+
+        const messagesRef = collection(db, "messages")
+
+        const [inputText, setInputText] = useState("")
+
+      function handleSubmit(e){
+        e.preventDefault()
+        setDoc(
+          doc(messagesRef),
+          {
+            text: inputText,
+            createdAt: serverTimestamp(),
+            userUid: auth.currentUser.uid,
+            userPhoto: auth.currentUser.photoURL 
+          }
+        )
+
+        setInputText("")
+
+      }
+
+      return(
+        <div>
+        <form 
+        onSubmit={handleSubmit}
+        className="flex gap-2 p-4">
+          <input 
+          type="text"
+          className="border border-gray-500 rounded-lg p-2 w-full"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          />
+          <button
+          type="submit"
+          className="bg-blue-500 p-2 text-white rounded-lg"
+          >
+          Gönder
+          </button>
+        </form>
+        </div>
       )
     }
 
